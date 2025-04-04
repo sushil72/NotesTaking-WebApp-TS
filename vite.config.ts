f3bbae0c80c2563c662f3cc1f7ddff1371885c4f
Susheel Kumar <sushilkumarrohidas0@gmail.com>
2025-04-04 11:46:13 +0530

    dploy on the github

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/mui-learning/',
   server: {
     port: 3000,
     open: true,
